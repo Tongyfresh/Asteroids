942c1159ef47c4f1160e302f25c6ac05a2e2c0a5
Tongyfresh <tip.bridge@gmail.com>
2025-01-03 16:18:55 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+from constants import *
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    fill_and_display()
+
+    
+def fill_and_display():
+    black = (0, 0, 0)
+    while True:
+        for event in pygame.event.get():  # Handle events
+            if event.type == pygame.QUIT:  # Check for quit event
+                return  # Exit the loop and end the function
+        screen.fill(black)
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
