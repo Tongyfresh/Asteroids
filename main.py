0a78bea92df59a7492b011f7787f3a1a1483ecdb
Tongyfresh <tip.bridge@gmail.com>
2025-01-03 16:44:25 -0700

    added circleshape

--- main.py
+++ main.py
@@ -12,12 +12,16 @@ def main():
     
 def fill_and_display():
     black = (0, 0, 0)
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():  # Handle events
             if event.type == pygame.QUIT:  # Check for quit event
                 return  # Exit the loop and end the function
         screen.fill(black)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 
 
 if __name__ == "__main__":
