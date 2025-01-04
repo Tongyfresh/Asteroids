0148c97e420c88b468e2d475fc47a3190cad4e92
Tongyfresh <tip.bridge@gmail.com>
2025-01-03 17:17:14 -0700

    added player to with vectoring and drawing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from circleshape import *
+from player import *
 from constants import *
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -7,10 +9,12 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    fill_and_display()
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
+    fill_and_display(player)
 
     
-def fill_and_display():
+def fill_and_display(player):
     black = (0, 0, 0)
     clock = pygame.time.Clock()
     dt = 0
@@ -19,6 +23,7 @@ def fill_and_display():
             if event.type == pygame.QUIT:  # Check for quit event
                 return  # Exit the loop and end the function
         screen.fill(black)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
